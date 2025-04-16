f76a43b3a5b639b62d215eb8302fb3bb4d13925b
Rigoberto Moreira <rigomoreirar@gmail.com>
2025-04-15 20:31:47 -0600

    Added microservice description

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!';
+    getMicroserviceName(): string {
+        return 'timecard-api-microservice 1.0v';
     }
 }
